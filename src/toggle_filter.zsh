640a864681a5d123f33ea890a5b2a9b08266aeaf
Pedro Vázquez <vanstrouble@proton.me>
2026-02-07 15:27:34 -0600

    Enhance toggle_filter.zsh to include display sleep option in the toggle response

--- src/toggle_filter.zsh
+++ src/toggle_filter.zsh
@@ -3,5 +3,5 @@
 if pgrep -x "caffeinate" >/dev/null 2>&1; then
     echo '{"items":[{"title":"Turn Off","subtitle":"Allow computer to sleep","arg":"off","icon":{"path":"icon.png"}}]}'
 else
-    echo '{"items":[{"title":"Turn On","subtitle":"Prevent sleep indefinitely","arg":"on","icon":{"path":"icon.png"}}]}'
+    echo '{"items":[{"title":"Turn On","subtitle":"Prevent sleep indefinitely","arg":"on","icon":{"path":"icon.png"},"mods":{"cmd":{"subtitle":"⌘ Allow display sleep","arg":"on","variables":{"display_sleep_allow":"true"}}}}]}'
 fi
