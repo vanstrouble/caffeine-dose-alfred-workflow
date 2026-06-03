30eb6dab24b24829a5cf7af9455189ff1ddfc240
Pedro Vázquez <vanstrouble@proton.me>
2026-06-03 13:45:03 -0600

    Enhance toggle_hotkey.zsh to include display sleep allowance in output

--- src/toggle_hotkey.zsh
+++ src/toggle_hotkey.zsh
@@ -1,7 +1,14 @@
 #!/bin/zsh --no-rcs
 
+allow_sleep="false"
+case "${hotkey_allow_sleep:l}" in
+    true|1|yes|on)
+        allow_sleep="true"
+        ;;
+esac
+
 if pgrep -x "caffeinate" >/dev/null 2>&1; then
-    echo -n "deactivate"
+    echo "{\"alfredworkflow\":{\"arg\":\"deactivate\",\"variables\":{\"display_sleep_allow\":\"$allow_sleep\"}}}"
 else
-    echo -n "indefinite"
+    echo "{\"alfredworkflow\":{\"arg\":\"indefinite\",\"variables\":{\"display_sleep_allow\":\"$allow_sleep\"}}}"
 fi
